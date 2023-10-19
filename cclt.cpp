1b93d27864fb8b8589bf9dda338ffd9a653f13b8
Nimo-123 <146323878+Nimo-123@users.noreply.github.com>
2023-10-19 22:54:24 +0300

    Add files via upload

---
+++ cclt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+int num1,num2,add,subs,div,mult;
+    cout <<" enter first numbers:"<<endl;
+    cin>> num1;
+    cout<<" enter second number:"<<endl;
+    cin>> num2;
+    add=num1+num2;
+subs=num1-num2;
+div= num1/num2;
+mult=num1*num2;
+    cout<< add <<endl;
+    cout<< subs <<endl;
+    cout<< div <<endl;
+    cout<< mult <<endl;
+     
+      
+    return 0;
+}
